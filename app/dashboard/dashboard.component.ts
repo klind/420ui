4832cba21c174478fcf61266f63f020382e0bad9
Kristian Lind <kristian.lind@allegiantair.com>
2016-10-02 18:16:33 -0700

    in progress

--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import {Router} from '@angular/router';
 
 @Component({
     selector: 'my-dashboard',
-    templateUrl: 'app/dashboard.component.html',
+    templateUrl: 'app/dashboard/dashboard.component.html',
     styleUrls: ['app/dashboard.component.css']
 
 })
